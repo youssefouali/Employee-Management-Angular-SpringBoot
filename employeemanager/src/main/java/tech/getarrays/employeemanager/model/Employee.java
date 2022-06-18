d3c89d39ec650ff32132d9375dcb4fabf883d9f1
youssefouali <youssef.ouali@esprit.tn>
2022-06-18 03:10:35 +0100

    first back end commit

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -0,0 +1,10 @@
+package tech.getarrays.employeemanager.model;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+}
