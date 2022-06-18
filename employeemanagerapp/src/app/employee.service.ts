9ad6a117e968c16bf7aec00fb7af1b8dc05564bb
youssefouali <youssef.ouali@esprit.tn>
2022-06-18 17:55:01 +0100

    STOP

--- employeemanagerapp/src/app/employee.service.ts
+++ employeemanagerapp/src/app/employee.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Observable } from "rxjs";
-import { Employee } from "./employee";
+import { Employee } from './employee';
 import { environment } from "src/environments/environment";
 
 @Injectable({
