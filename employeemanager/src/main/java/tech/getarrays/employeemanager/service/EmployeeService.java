3da667958d7afa47f3d02017ff2a0e13c9705c09
youssefouali <youssef.ouali@esprit.tn>
2022-06-18 03:44:05 +0100

    Employee Repo,Service and app props

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package tech.getarrays.employeemanager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.getarrays.employeemanager.model.Employee;
+import tech.getarrays.employeemanager.repo.EmployeeRepo;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+}
